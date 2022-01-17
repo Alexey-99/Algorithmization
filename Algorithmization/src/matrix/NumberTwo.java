0fd0297f48f803d05e781be5693280e03144ae3e
Acer <Acer@house-pc>
2022-01-17 15:22:03 +0200

    *

--- Algorithmization/src/matrix/NumberTwo.java
+++ Algorithmization/src/matrix/NumberTwo.java
@@ -1,8 +1,12 @@
+/* Дана квадратная матрица. Вывести на экран элементы, стоящие на диагонали. */
+
+
 package matrix;
 
 public class NumberTwo {
 
 	public static void main(String[] args) {
+		
 		
 
 	}
