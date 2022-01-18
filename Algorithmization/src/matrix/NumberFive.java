2c7634d04feb12e9db5be11effcc82dae35d49de
Acer <Acer@house-pc>
2022-01-18 15:26:24 +0200

    *

---
+++ Algorithmization/src/matrix/NumberFive.java
@@ -0,0 +1,44 @@
+/*Сформировать квадратную матрицу порядка n по заданному образцу(n - четное): 
+ * 1 1 1 1 1 ... 1 1 1 
+ * 2 2 2 2 2 ... 2 2 0 
+ * 3 3 3 3 3 ... 3 0 0 
+ * . . . . . .   . . .  
+ * . . . . .  .  . . .
+ * . . . . .   . . . .
+ * n-1 n-1 0 0 0 0 0 0 
+ * n 0 0 0 0 0 0 0 0 0 
+ * */
+
+package matrix;
+
+import java.util.Random;
+
+public class NumberFive {
+
+	public static void main(String[] args) {
+		int n = matrixLengthInitialization();
+		int[][] matrix = new int[n][n];
+		printMatrix(matrix);
+	}
+
+	private static int matrixLengthInitialization() {
+		int length = 0;
+		Random rand = new Random();
+		while ((length % 2 != 0) || (length == 0)) {
+			length = rand.nextInt(10);
+		}
+		return length;
+	}
+
+	private static void printMatrix(int[][] matrix) {
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				if (j < matrix[i].length - i) {
+					matrix[i][j] = i + 1;
+				}
+				System.out.print(matrix[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
--- Algorithmization/src/matrix/NumberFour.java
+++ Algorithmization/src/matrix/NumberFour.java
@@ -1,4 +1,12 @@
 /* Сформировать квадратную матрицу порядка n по заданному образцу(n - четное): 
+ * 1  2   3  ... n 
+ * n n-1 n-2 ... 1 
+ * 1  2   3  ... n 
+ * n n-1 n-2 ... 1  
+ * . . . . . .   .
+ * . . . . .  .  .
+ * . . . . .   . .
+ * n n-1 n-2 ... 1 
  * */
 
 package matrix;
@@ -8,37 +16,29 @@ import java.util.Random;
 public class NumberFour {
 
 	public static void main(String[] args) {
-		Random rand = new Random();
-		int y = rand.nextInt(30) + 1;
-		int x = rand.nextInt(30) + 1;
-		int[][] matrix = new int[y][x];
-		matrix = matrixCreationRandNumber(matrix);
-		System.out.println();
+		int n = matrixLengthInitialization();
+		int[][] matrix = new int[n][n];
 		printMatrix(matrix);
 	}
 
-	private static int[][] matrixCreationRandNumber(int[][] matrix) {
-		System.out.println("Первоначальная матрица.");
+	private static int matrixLengthInitialization() {
+		int length = 0;
 		Random rand = new Random();
-		for (int i = 0; i < matrix.length; i++) {
-			for (int j = 0; j < matrix[i].length; j++) {
-				matrix[i][j] = rand.nextInt(10);
-				System.out.print(matrix[i][j] + " ");
-			}
-			System.out.println();
+		while ((length % 2 != 0) || (length == 0)) {
+			length = rand.nextInt(10);
 		}
-		return matrix;
+		return length;
 	}
 
 	private static void printMatrix(int[][] matrix) {
 		for (int i = 0; i < matrix.length; i++) {
-			if ((i == 0) || (i % 2 != 0)) {
+			if ((i == 0) || (i % 2 == 0)) {
 				for (int j = 0; j < matrix[i].length; j++) {
-					System.out.print(matrix[i][j] + " ");
+					System.out.print(matrix[i][j] + (j + 1) + " ");
 				}
-			} else if ((i != 0) && (i % 2 == 0)) {
+			} else if ((i != 0) && (i % 2 != 0)) {
 				for (int j = matrix[i].length - 1; j >= 0; j--) {
-					System.out.print(matrix[i][j] + " ");
+					System.out.print(matrix[i][j] + (j + 1) + " ");
 				}
 			}
 			System.out.println();
