8db1849fff384d7823c96f096c9bacd76991d943
Acer <Acer@house-pc>
2022-01-19 20:15:01 +0200

    *

---
+++ Algorithmization/src/matrix/NumberTwelve.java
@@ -0,0 +1,12 @@
+/* Отсортировать строки матрицы по возрастанию и убыванию значений элементов.*/
+
+package matrix;
+
+public class NumberTwelve {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
