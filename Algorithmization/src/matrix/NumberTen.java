a1ec13b6706764fffea88dca70ce6588f16e89bf
Acer <Acer@house-pc>
2022-01-19 19:51:30 +0200

    *

--- Algorithmization/src/matrix/NumberTen.java
+++ Algorithmization/src/matrix/NumberTen.java
@@ -10,7 +10,8 @@ public class NumberTen {
 		int length = matrixLengthInitialization();
 		int[][] matrix = new int[length][length];
 		matrix = fillingMatrixRandomNumbers(matrix);
-
+		findPositiveElementsMainDiagonalFromLeftToRight(matrix);
+		findPositiveElementsMainDiagonalFromRightToLeft(matrix);
 	}
 
 	private static int matrixLengthInitialization() {
@@ -31,10 +32,31 @@ public class NumberTen {
 			}
 			System.out.println();
 		}
+		System.out.println();
 		return matrix;
 	}
 
 	private static void findPositiveElementsMainDiagonalFromLeftToRight(int[][] matrix) {
-		
+		System.out.println("Положительные элементы главной диагонали (с лева на право) квадратной матрицы.");
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = i; j <= i; j++) {
+				if (matrix[i][j] >= 0) {
+					System.out.print(matrix[i][j] + " ");
+				}
+
+			}
+		}
+		System.out.println();
+	}
+
+	private static void findPositiveElementsMainDiagonalFromRightToLeft(int[][] matrix) {
+		System.out.println("Положительные элементы главной диагонали (с право на лево) квадратной матрицы.");
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = matrix[i].length - 1 - i; j >= matrix[i].length - 1 - i; j--) {
+				if (matrix[i][j] > 0) {
+					System.out.print(matrix[i][j] + " ");
+				}
+			}
+		}
 	}
 }
