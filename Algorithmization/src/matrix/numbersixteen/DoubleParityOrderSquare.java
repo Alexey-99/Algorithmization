5ce649936f6e58ecd9da4934f399c843333e5fe9
Alexey <koroza.alexey99@gmail.com>
2022-01-23 15:40:14 +0200

    *

---
+++ Algorithmization/src/matrix/numbersixteen/DoubleParityOrderSquare.java
@@ -0,0 +1,71 @@
+package matrix.numbersixteen;
+
+public class DoubleParityOrderSquare {
+
+	public void buildingDoubleParityOrderSquare(int size) {
+		int[][] matrix = new int[size][size];
+		int magicConstant = calculateMagicConstant(size);
+		System.out.println("Магическая сумма:" + magicConstant);
+		matrix = buildingMagicSquareCornerAndCenterElements(matrix);
+		buildingMagicSquareExtremeElements(matrix);
+		printMagicSquare(matrix);
+	}
+
+	private int calculateMagicConstant(int size) {
+		int magicConstant = (size * ((int) Math.pow(size, 2) + 1)) / 2;
+		return magicConstant;
+	}
+
+	private int[][] buildingMagicSquareCornerAndCenterElements(int[][] matrix) {
+		int beginningCentralSquareTop = matrix.length / 4 - 1;
+		int beginningCentralSquareBottom = matrix.length - beginningCentralSquareTop - 1;
+		int beginningCentralSquareLeft = beginningCentralSquareTop;
+		int beginningCentralSquareRight = beginningCentralSquareBottom;
+		int count = 0;
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				count++;
+				if ((i == 0 || i == matrix.length - 1) && ((j == 0) || (j == matrix[i].length - 1))) {
+					matrix[i][j] = count;
+				} else if ((i > beginningCentralSquareTop && i < beginningCentralSquareBottom)
+						&& (j > beginningCentralSquareLeft && j < beginningCentralSquareRight)) {
+					matrix[i][j] = count;
+				}
+			}
+		}
+		return matrix;
+	}
+
+	private int[][] buildingMagicSquareExtremeElements(int[][] matrix) {
+		int beginningCentralSquareTop = matrix.length / 4 - 1;
+		int beginningCentralSquareBottom = matrix.length - beginningCentralSquareTop - 1;
+		int beginningCentralSquareLeft = beginningCentralSquareTop + 1;
+		int beginningCentralSquareRight = beginningCentralSquareBottom - 1;
+		int count = 0;
+		for (int i = matrix.length - 1; i >= 0; i--) {
+			for (int j = matrix[i].length - 1; j >= 0; j--) {
+				count++;
+				if ((i == 0 || i == matrix.length - 1) && (j > 0 && j < matrix[i].length - 1)) {
+					matrix[i][j] = count;
+				} else if ((i > beginningCentralSquareTop && i < beginningCentralSquareBottom)
+						&& (j < beginningCentralSquareLeft || j > beginningCentralSquareRight)) {
+					matrix[i][j] = count;
+				} else if ((i > 0 && i <= beginningCentralSquareTop)
+						|| (i < matrix.length - 1 && i >= beginningCentralSquareBottom)) {
+					matrix[i][j] = count;
+				}
+			}
+		}
+		return matrix;
+	}
+
+	private void printMagicSquare(int[][] matrix) {
+		System.out.println();
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				System.out.print(matrix[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
--- Algorithmization/src/matrix/numbersixteen/NamberSixteen.java
+++ Algorithmization/src/matrix/numbersixteen/NamberSixteen.java
@@ -32,78 +32,12 @@ public class NamberSixteen {
 	private static void buildingAndPrintMagicSquare() {
 		int size = matrixLengthInitialization();
 		if ((size % 2 == 0) && (size % 4 == 0)) { // 2
-			buildingDoubleParityOrderSquare(size);
+			DoubleParityOrderSquare doubleParityOrderSquare = new DoubleParityOrderSquare();
+			doubleParityOrderSquare.buildingDoubleParityOrderSquare(size);
 		} else if ((size % 2 == 0) && (size % 4 != 0)) { // 1
 
-		} else if (size % 2 != 0) {
+		} else if (size % 2 != 0) { // 3
 
-		}
-	}
-
-	private static void buildingDoubleParityOrderSquare(int size) {
-		int[][] matrix = new int[size][size];
-		int magicConstant = calculateMagicConstant(size);
-		System.out.println("Магическая сумма:" + magicConstant);
-		matrix = buildingMagicSquareCornerAndCenterElements(matrix);
-		buildingMagicSquareExtremeElements(matrix);
-		printMagicSquare(matrix);
-	}
-
-	private static int calculateMagicConstant(int size) {
-		int magicConstant = (size * ((int) Math.pow(size, 2) + 1)) / 2;
-		return magicConstant;
-	}
-
-	private static int[][] buildingMagicSquareCornerAndCenterElements(int[][] matrix) {
-		int beginningCentralSquareTop = matrix.length / 4 - 1;
-		int beginningCentralSquareBottom = matrix.length - beginningCentralSquareTop - 1;
-		int beginningCentralSquareLeft = beginningCentralSquareTop;
-		int beginningCentralSquareRight = beginningCentralSquareBottom;
-		int count = 0;
-		for (int i = 0; i < matrix.length; i++) {
-			for (int j = 0; j < matrix[i].length; j++) {
-				count++;
-				if ((i == 0 || i == matrix.length - 1) && ((j == 0) || (j == matrix[i].length - 1))) {
-					matrix[i][j] = count;
-				} else if ((i > beginningCentralSquareTop && i < beginningCentralSquareBottom)
-						&& (j > beginningCentralSquareLeft && j < beginningCentralSquareRight)) {
-					matrix[i][j] = count;
-				}
-			}
-		}
-		return matrix;
-	}
-
-	private static int[][] buildingMagicSquareExtremeElements(int[][] matrix) {
-		int beginningCentralSquareTop = matrix.length / 4 - 1;
-		int beginningCentralSquareBottom = matrix.length - beginningCentralSquareTop - 1;
-		int beginningCentralSquareLeft = beginningCentralSquareTop + 1;
-		int beginningCentralSquareRight = beginningCentralSquareBottom - 1;
-		int count = 0;
-		for (int i = matrix.length - 1; i >= 0; i--) {
-			for (int j = matrix[i].length - 1; j >= 0; j--) {
-				count++;
-				if ((i == 0 || i == matrix.length - 1) && (j > 0 && j < matrix[i].length - 1)) {
-					matrix[i][j] = count;
-				} else if ((i > beginningCentralSquareTop && i < beginningCentralSquareBottom)
-						&& (j < beginningCentralSquareLeft || j > beginningCentralSquareRight)) {
-					matrix[i][j] = count;
-				} else if ((i > 0 && i <= beginningCentralSquareTop)
-						|| (i < matrix.length - 1 && i >= beginningCentralSquareBottom)) {
-					matrix[i][j] = count;
-				}
-			}
-		}
-		return matrix;
-	}
-
-	private static void printMagicSquare(int[][] matrix) {
-		System.out.println();
-		for (int i = 0; i < matrix.length; i++) {
-			for (int j = 0; j < matrix[i].length; j++) {
-				System.out.print(matrix[i][j] + " ");
-			}
-			System.out.println();
 		}
 	}
 }
