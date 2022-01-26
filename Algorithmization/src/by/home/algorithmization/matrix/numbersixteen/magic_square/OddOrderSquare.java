35161f6b6c974503cc751cf1d757aeebb271c725
Alexey <koroza.alexey99@gmail.com>
2022-01-26 15:59:47 +0200

    *

--- Algorithmization/src/by/home/algorithmization/matrix/numbersixteen/magic_square/OddOrderSquare.java
+++ Algorithmization/src/by/home/algorithmization/matrix/numbersixteen/magic_square/OddOrderSquare.java
@@ -5,25 +5,34 @@ public class OddOrderSquare {
 	public void buildOddOrderSquare(int size) {
 		int[][] matrixEnlarged = new int[size + (size - 1)][size + (size - 1)];
 		int[][] matrix = new int[size][size];
+		buildOddOrderSquareEnlarged(matrixEnlarged, matrix);
+		printMagicSquare(matrixEnlarged);
+
+	}
+
+	private void buildOddOrderSquareEnlarged(int[][] matrixEnlarged, int[][] matrix) {
 		double iCount = (double) matrixEnlarged.length / 2 - 0.5;
 		int count = 0;
 		int jCount = 0;
-		int iMax = (int) (iCount + 3);
-		
 		for (int f = 0; f < matrix.length; f++) {
 			jCount = f;
 			for (int i = (int) iCount; i >= iCount - (matrix.length - 1); i--) {
 				count++;
 				for (int j = jCount; j <= jCount; j++) {
 					matrixEnlarged[i][j] = count;
-					printMagicSquare(matrixEnlarged);
 				}
 				jCount++;
 			}
 			iCount++;
-			
 		}
-		
+		iCount = (double) matrixEnlarged.length / 2 - 0.5;
+		printMagicSquare(matrixEnlarged);
+		matrixEnlarged[matrixEnlarged.length - 2][(int) iCount] = matrixEnlarged[0][(int) iCount];
+		printMagicSquare(matrixEnlarged);
+		matrixEnlarged[(int) iCount][1] = matrixEnlarged[(int) iCount][matrixEnlarged.length - 1];
+		printMagicSquare(matrixEnlarged);
+		matrixEnlarged[1][(int) iCount] = matrixEnlarged[matrixEnlarged.length - 1][(int) iCount];
+		printMagicSquare(matrixEnlarged);
 	}
 
 	private void printMagicSquare(int[][] matrix) {
