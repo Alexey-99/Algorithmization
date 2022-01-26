24a0887de0c76c4012744bc9da1c990b7e303bb2
Alexey <koroza.alexey99@gmail.com>
2022-01-26 17:49:58 +0200

    *

--- Algorithmization/src/by/home/algorithmization/matrix/numbersixteen/magic_square/OddOrderSquare.java
+++ Algorithmization/src/by/home/algorithmization/matrix/numbersixteen/magic_square/OddOrderSquare.java
@@ -7,32 +7,92 @@ public class OddOrderSquare {
 		int[][] matrix = new int[size][size];
 		buildOddOrderSquareEnlarged(matrixEnlarged, matrix);
 		printMagicSquare(matrixEnlarged);
-
+		transferFromMagnifiedSquareToMagicSquare(matrixEnlarged, matrix);
+		printMagicSquare(matrix);
 	}
 
 	private void buildOddOrderSquareEnlarged(int[][] matrixEnlarged, int[][] matrix) {
-		double iCount = (double) matrixEnlarged.length / 2 - 0.5;
+		double iStartCount = (double) matrixEnlarged.length / 2 - 0.5;
 		int count = 0;
 		int jCount = 0;
 		for (int f = 0; f < matrix.length; f++) {
 			jCount = f;
-			for (int i = (int) iCount; i >= iCount - (matrix.length - 1); i--) {
+			for (int i = (int) iStartCount; i >= iStartCount - (matrix.length - 1); i--) {
 				count++;
 				for (int j = jCount; j <= jCount; j++) {
 					matrixEnlarged[i][j] = count;
 				}
 				jCount++;
 			}
-			iCount++;
+			iStartCount++;
 		}
-		iCount = (double) matrixEnlarged.length / 2 - 0.5;
+		fillingFourCellsSquare(matrixEnlarged, matrix);
+	}
+
+	private void fillingFourCellsSquare(int[][] matrixEnlarged, int[][] matrix) {
+		topMarginCheck(matrixEnlarged, matrix);
 		printMagicSquare(matrixEnlarged);
-		matrixEnlarged[matrixEnlarged.length - 2][(int) iCount] = matrixEnlarged[0][(int) iCount];
+		rightMarginCheck(matrixEnlarged, matrix);
 		printMagicSquare(matrixEnlarged);
-		matrixEnlarged[(int) iCount][1] = matrixEnlarged[(int) iCount][matrixEnlarged.length - 1];
+		bottomMarginCheck(matrixEnlarged, matrix);
 		printMagicSquare(matrixEnlarged);
-		matrixEnlarged[1][(int) iCount] = matrixEnlarged[matrixEnlarged.length - 1][(int) iCount];
+		leftMarginCheck(matrixEnlarged, matrix);
 		printMagicSquare(matrixEnlarged);
+
+	}
+
+	private void topMarginCheck(int[][] matrixEnlarged, int[][] matrix) {
+		int iStartCount = (matrixEnlarged.length - matrix.length) / 2;
+		for (int i = 0; i < iStartCount; i++) {
+			for (int j = 0; j < matrixEnlarged.length; j++) {
+				if (matrixEnlarged[i][j] != 0) {
+					matrixEnlarged[i + 5][j] = matrixEnlarged[i][j];
+				}
+			}
+		}
+	}
+
+	private void rightMarginCheck(int[][] matrixEnlarged, int[][] matrix) {
+		int iStartCount = (matrixEnlarged.length - matrix.length) / 2;
+		for (int i = iStartCount; i < matrixEnlarged.length; i++) {
+			for (int j = matrixEnlarged.length - iStartCount; j < matrixEnlarged.length; j++) {
+				if (matrixEnlarged[i][j] != 0) {
+					matrixEnlarged[i][j - 5] = matrixEnlarged[i][j];
+				}
+			}
+		}
+	}
+
+	private void bottomMarginCheck(int[][] matrixEnlarged, int[][] matrix) {
+		int iStartCount = (matrixEnlarged.length - matrix.length) / 2;
+		for (int i = matrixEnlarged.length - iStartCount; i < matrixEnlarged.length; i++) {
+			for (int j = 0; j < matrixEnlarged.length; j++) {
+				if (matrixEnlarged[i][j] != 0) {
+					matrixEnlarged[i - 5][j] = matrixEnlarged[i][j];
+				}
+			}
+		}
+	}
+
+	private void leftMarginCheck(int[][] matrixEnlarged, int[][] matrix) {
+		int iStartCount = (matrixEnlarged.length - matrix.length) / 2;
+		for (int i = iStartCount; i < matrixEnlarged.length; i++) {
+			for (int j = 0; j < iStartCount; j++) {
+				if (matrixEnlarged[i][j] != 0) {
+					matrixEnlarged[i][j + 5] = matrixEnlarged[i][j];
+				}
+			}
+		}
+	}
+
+	private void transferFromMagnifiedSquareToMagicSquare(int[][] matrixEnlarged, int[][] matrix) {
+		int iCount = (matrixEnlarged.length - matrix.length) / 2;
+		for (int i = iCount; i < matrixEnlarged.length - 1; i++) {
+			for (int j = (matrixEnlarged.length - matrix.length) / 2; j < matrixEnlarged[i].length - 1; j++) {
+				matrix[i - iCount][j - iCount] = matrixEnlarged[i][j];
+				printMagicSquare(matrix);
+			}
+		}
 	}
 
 	private void printMagicSquare(int[][] matrix) {
