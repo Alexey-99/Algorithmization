760ce5b798ebaca135268935bfdc83035578156c
Alexey <koroza.alexey99@gmail.com>
2022-01-26 14:32:51 +0200

    *

--- Algorithmization/src/by/home/algorithmization/matrix/numbersixteen/magic_square/OddOrderSquare.java
+++ Algorithmization/src/by/home/algorithmization/matrix/numbersixteen/magic_square/OddOrderSquare.java
@@ -5,13 +5,18 @@ public class OddOrderSquare {
 	public void buildOddOrderSquare(int size) {
 		int[][] matrixEnlarged = new int[size + (size - 1)][size + (size - 1)];
 		int[][] matrix = new int[size][size];
+		double iCount = (double) matrixEnlarged.length / 2 - 0.5;
 		int count = 0;
-		for (int i = 0; i < matrixEnlarged.length; i++) {
-			for (int j = 0; j < matrixEnlarged[i].length; j++) {
-				count++;
+		for (int i = (int) iCount; i >= 0; i--) {
+			count++;
+			for (int j = count - 1; j <= count - 1; j++) {
+				matrixEnlarged[i][j] = count;
+				printMagicSquare(matrixEnlarged);
 				
 			}
 		}
+		
+
 	}
 
 	private void printMagicSquare(int[][] matrix) {
