b8e7352e78b7681f9d97082c802f53b51f2e8f0a
Alexey <koroza.alexey99@gmail.com>
2022-01-26 18:00:29 +0200

    *

--- Algorithmization/src/by/home/algorithmization/matrix/numbersixteen/main/NamberSixteen.java
+++ Algorithmization/src/by/home/algorithmization/matrix/numbersixteen/main/NamberSixteen.java
@@ -41,7 +41,7 @@ public class NamberSixteen {
 		} else if ((size % 2 == 0) && (size % 4 != 0)) { // TODO 1
 			SingleParityOrderSquare singleParityOrderSquare = new SingleParityOrderSquare();
 			singleParityOrderSquare.buildSingleParityOrderSquare(size);
-		} else if (size % 2 != 0) { // TODO 3
+		} else if (size % 2 != 0) { // 3
 			OddOrderSquare oddOrderSquare = new OddOrderSquare();
 			oddOrderSquare.buildOddOrderSquare(size);
 		}
