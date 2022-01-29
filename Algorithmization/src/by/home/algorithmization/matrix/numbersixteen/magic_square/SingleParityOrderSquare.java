2dfa0f4841c101a111e8c46fc7578938cf9cf711
Alexey <koroza.alexey99@gmail.com>
2022-01-29 19:50:39 +0200

    *

--- Algorithmization/src/by/home/algorithmization/matrix/numbersixteen/magic_square/SingleParityOrderSquare.java
+++ Algorithmization/src/by/home/algorithmization/matrix/numbersixteen/magic_square/SingleParityOrderSquare.java
@@ -121,7 +121,7 @@ public class SingleParityOrderSquare {
 		System.out.println();
 		for (int i = 0; i < matrix.length; i++) {
 			for (int j = 0; j < matrix[i].length; j++) {
-				System.out.printf("%2d ", matrix[i][j]);
+				System.out.printf("%3d ", matrix[i][j]);
 			}
 			System.out.println();
 		}
