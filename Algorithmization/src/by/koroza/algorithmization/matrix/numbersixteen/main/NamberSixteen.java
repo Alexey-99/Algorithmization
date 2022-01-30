377acc780f0de9b589c73fe8eb667ae271178022
Alexey <koroza.alexey99@gmail.com>
2022-01-30 16:45:22 +0200

    *

--- Algorithmization/src/by/koroza/algorithmization/matrix/numbersixteen/main/NamberSixteen.java
+++ Algorithmization/src/by/koroza/algorithmization/matrix/numbersixteen/main/NamberSixteen.java
@@ -19,20 +19,57 @@ import by.koroza.algorithmization.matrix.numbersixteen.magicsquare.OddOrderSquar
 import by.koroza.algorithmization.matrix.numbersixteen.magicsquare.SingleParityOrderSquare;
 
 public class NamberSixteen {
-	
-	// TODO ПРОВЕРИТЬ ЧТОБЫ НЕ БЫЛО МАГИЧЕСКИХ ЧИСЕЛ 
+
+	// TODO ПРОВЕРИТЬ ЧТОБЫ НЕ БЫЛО МАГИЧЕСКИХ ЧИСЕЛ
 
 	public static void main(String[] args) {
 		buildingAndPrintMagicSquare();
 	}
 
-	private static int matrixLengthInitialization() { //TODO ПРОВЕРКА ЧТО ВВЕДЕНО ЧИСЛО НЕ МЕНЕЕ 3 И ЧИСЛО ЭТО ВООБЩЕ
-		int length = 0;
+	private static int matrixLengthInitialization() { 
+		int size = 0;
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Введите размер (не менее 3-ёх) магического квадрата ");
-		length = scan.nextInt();
+		boolean flag = false;
+		while (flag == false) {
+			System.out.println("Введите размер (не менее 3-ёх) магического квадрата ");
+			String line = scan.nextLine();
+			flag = checkingNumbersInString(line);
+			if (flag == true) {
+				flag = checkingForPresenceNumberInStringGreaterThanThree(line);
+				if (flag == true) {
+					size = Integer.parseInt(line);
+				}
+			}
+		}
 		scan.close();
-		return length;
+		return size;
+	}
+
+	private static boolean checkingNumbersInString(String line) {
+		boolean flag = true;
+		String[] arrayLine = line.split("");
+		for (String element : arrayLine) {
+			if ((element.equals("1") || element.equals("2")) || (element.equals("3") || element.equals("4"))
+					|| (element.equals("5") || element.equals("6"))
+					|| (element.equals("7") || element.equals("8") || (element.equals("9") || element.equals("0")))) {
+			} else {
+				flag = false;
+			}
+		}
+		if (flag == false) {
+			System.out.println("Введите, пожалуйста, коректно размер магического квадрата");
+		}
+		return flag;
+	}
+
+	private static boolean checkingForPresenceNumberInStringGreaterThanThree(String line) {
+		boolean flag = true;
+		int size = Integer.parseInt(line);
+		if (size < 3) {
+			flag = false;
+		}
+		System.out.println("Введите, пожалуйста, размер магического квадрата, больше 2");
+		return flag;
 	}
 
 	private static void buildingAndPrintMagicSquare() {
@@ -40,7 +77,7 @@ public class NamberSixteen {
 		if ((size % 2 == 0) && (size % 4 == 0)) {
 			DoubleParityOrderSquare doubleParityOrderSquare = new DoubleParityOrderSquare();
 			doubleParityOrderSquare.buildingDoubleParityOrderSquare(size);
-		} else if ((size % 2 == 0) && (size % 4 != 0)) { 
+		} else if ((size % 2 == 0) && (size % 4 != 0)) {
 			SingleParityOrderSquare singleParityOrderSquare = new SingleParityOrderSquare();
 			singleParityOrderSquare.buildSingleParityOrderSquare(size);
 		} else if (size % 2 != 0) {
--- Algorithmization/src/by/koroza/algorithmization/sorting/NumberOne.java
+++ Algorithmization/src/by/koroza/algorithmization/sorting/NumberOne.java
@@ -8,7 +8,5 @@ public class NumberOne {
 
 	public static void main(String[] args) {
 		
-
 	}
-
 }
