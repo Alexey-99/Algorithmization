765df4fd1d2e3c86f3341ea86f4680542761d772
Alexey <koroza.alexey99@gmail.com>
2022-01-30 17:27:06 +0200

    *

--- Algorithmization/src/by/koroza/algorithmization/sorting/NumberOne.java
+++ Algorithmization/src/by/koroza/algorithmization/sorting/NumberOne.java
@@ -4,9 +4,30 @@
 
 package by.koroza.algorithmization.sorting;
 
+import java.util.Random;
+
 public class NumberOne {
 
 	public static void main(String[] args) {
-		
+		int k = 2;
+		int[] array1 = new int[matrixLengthInitialization()];
+		int[] array2 = new int[matrixLengthInitialization()];
+		assigningRandomNumbersArray(array1);
+		assigningRandomNumbersArray(array2);
 	}
+
+	private static int matrixLengthInitialization() {
+		Random rand = new Random();
+		int length = 0;
+		length = rand.nextInt(20) + 1;
+		return length;
+	}
+	
+	private static void assigningRandomNumbersArray(int[] array) {
+		Random rand = new Random();
+		for (int i = 0; i < array.length; i++) {
+			array[i] = rand.nextInt(10) + 1;
+		}
+	}
+
 }
