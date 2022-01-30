52f85e592d36270dae22ac407933d8e6f42d2e8d
Alexey <koroza.alexey99@gmail.com>
2022-01-30 19:50:52 +0200

    *

--- Algorithmization/src/by/koroza/algorithmization/sorting/NumberOne.java
+++ Algorithmization/src/by/koroza/algorithmization/sorting/NumberOne.java
@@ -1,19 +1,25 @@
-/*Заданы два одномерных массива с различным количеством элементов и натуральное число k. 
+/* Заданы два одномерных массива с различным количеством элементов и натуральное число k. 
  * Объединить их в один массив, включив второй массив между k-м и (k+1)-м элементами первого, 
  * при этом не используя дополнительный массив. */
 
 package by.koroza.algorithmization.sorting;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class NumberOne {
 
 	public static void main(String[] args) {
-		int k = 2;
-		int[] array1 = new int[matrixLengthInitialization()];
-		int[] array2 = new int[matrixLengthInitialization()];
-		assigningRandomNumbersArray(array1);
-		assigningRandomNumbersArray(array2);
+		int lengthFirstArray = matrixLengthInitialization();
+		int numberK = findingRandomNumber(lengthFirstArray);
+		int lengthSecondArray = matrixLengthInitialization();
+		int[] firstArray = new int[lengthFirstArray + lengthSecondArray];
+		int[] secondArray = new int[lengthSecondArray];
+		assigningRandomNumbersArray(firstArray, lengthFirstArray);
+		System.out.println(Arrays.toString(firstArray));
+		assigningRandomNumbersArray(secondArray, lengthSecondArray);
+		System.out.println(Arrays.toString(secondArray));
+		unionArrays(firstArray, secondArray, numberK);
 	}
 
 	private static int matrixLengthInitialization() {
@@ -22,12 +28,41 @@ public class NumberOne {
 		length = rand.nextInt(20) + 1;
 		return length;
 	}
-	
-	private static void assigningRandomNumbersArray(int[] array) {
+
+	private static void assigningRandomNumbersArray(int[] array, int lengthArray) {
 		Random rand = new Random();
-		for (int i = 0; i < array.length; i++) {
+		for (int i = 0; i < lengthArray; i++) {
 			array[i] = rand.nextInt(10) + 1;
 		}
 	}
 
+	private static int findingRandomNumber(int lengthFirstArray) {
+		Random rand = new Random();
+		int k = 0;
+		do {
+			k = rand.nextInt(lengthFirstArray);
+		} while (k > lengthFirstArray - 2);
+		System.out.println(k + " ");
+		return k;
+	}
+
+	private static void unionArrays(int[] firstArray, int[] secondArray, int numberK) {
+		int i = 0;
+		for (i = 0; i < firstArray.length; i++) {
+			if (i == numberK + 1) {
+				for (int j = 0; j < secondArray.length; j++) {
+					for (int f = firstArray.length - secondArray.length - 1 + j; f >= numberK + 1 + j; f--) {
+						int tmp = firstArray[f];
+						firstArray[f] = firstArray[f + 1];
+						firstArray[f + 1] = tmp;
+					}
+					firstArray[i + j] = secondArray[j];
+					if (j == secondArray.length - 1) {
+						i += j;
+					}
+				}
+			}
+		}
+		System.out.println(Arrays.toString(firstArray));
+	}
 }
