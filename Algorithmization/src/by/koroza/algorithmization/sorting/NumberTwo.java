80fcc6ba362084d30a74b63bdd276f5123916b46
Alexey <koroza.alexey99@gmail.com>
2022-01-30 20:09:42 +0200

    *

---
+++ Algorithmization/src/by/koroza/algorithmization/sorting/NumberTwo.java
@@ -0,0 +1,15 @@
+/* Даны две последовательности a1 <= a2 <= ... <= an и b1 <= b2 <= ... <= bm. 
+ * Образовать из них новую последовательность чисел так, чтобы она тоже была неубывающей. 
+ * Примечание. Дополнительный массив не использовать. */
+
+
+package by.koroza.algorithmization.sorting;
+
+public class NumberTwo {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
