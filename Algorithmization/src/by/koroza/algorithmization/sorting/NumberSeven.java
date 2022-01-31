0d80076c72793058d6f68fca32e5e5c9aa19af82
Alexey <koroza.alexey99@gmail.com>
2022-01-31 18:05:52 +0200

    *

--- Algorithmization/src/by/koroza/algorithmization/sorting/NumberSeven.java
+++ Algorithmization/src/by/koroza/algorithmization/sorting/NumberSeven.java
@@ -14,21 +14,45 @@ public class NumberSeven {
 		int lengthSecondArray = arrayLengthInitialization();
 		double[] firstArray = new double[lengthfirstArray + lengthSecondArray];
 		assigningRandomNumbersArray(firstArray, lengthfirstArray);
+		sortArray(firstArray);
+		printArray(firstArray);
 		double[] secondArray = new double[lengthSecondArray];
-		
+		assigningRandomNumbersArray(secondArray, lengthSecondArray);
+		sortArray(secondArray);
+		printArray(secondArray);
 	}
-	
+
 	private static int arrayLengthInitialization() {
 		Random rand = new Random();
 		int length = 0;
 		length = rand.nextInt(20) + 1;
 		return length;
 	}
-	
+
 	private static void assigningRandomNumbersArray(double[] array, int lengthfirstArray) {
 		Random rand = new Random();
 		for (int i = 0; i < lengthfirstArray; i++) {
 			array[i] = rand.nextInt(10);
 		}
+	}
+
+	private static void sortArray(double[] array) {
+		for (int i = 0; i < array.length; i++) {
+			for (int j = i; j < array.length; j++) {
+				if (array[i] > array[j]) {
+					double tmp = array[i];
+					array[i] = array[j];
+					array[j] = tmp;
+				}
+			}
+		}
+	}
+	
+	private static void printArray(double[] array) {
+		System.out.println();
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
+		System.out.println();
 	}
 }
