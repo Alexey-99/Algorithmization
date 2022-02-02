1583a47e48c1483ff7fb768ad6a0782b52817fcb
Alexey <koroza.alexey99@gmail.com>
2022-02-02 18:16:03 +0200

    *

---
+++ Algorithmization/src/by/koroza/algorithmization/sorting/NumberEight.java
@@ -0,0 +1,19 @@
+/*Даны дроби 
+ * 
+ * P1  P2       Pn
+ * - , - , ... ,-   (Pi и Qi - натуральные).
+ * Q1  Q2       Qn
+ * 
+ * Составить программу, которая приводит эти дроби к общему знаменателю и упорядочивает их в порядке возрастания.
+ */
+
+package by.koroza.algorithmization.sorting;
+
+public class NumberEight {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
