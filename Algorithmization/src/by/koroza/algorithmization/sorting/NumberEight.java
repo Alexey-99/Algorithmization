deef07fa5c952817434eede237f56b555296a606
Alexey <koroza.alexey99@gmail.com>
2022-02-02 19:07:32 +0200

    *

--- Algorithmization/src/by/koroza/algorithmization/sorting/NumberEight.java
+++ Algorithmization/src/by/koroza/algorithmization/sorting/NumberEight.java
@@ -9,11 +9,62 @@
 
 package by.koroza.algorithmization.sorting;
 
+import java.util.Random;
+
 public class NumberEight {
 
 	public static void main(String[] args) {
-		
-
+		int lengthArrayPAndQ = arrayLengthInitialization();
+		int[] arrayP = new int[lengthArrayPAndQ];
+		int[] arrayQ = new int[lengthArrayPAndQ];
+		assigningRandomNumbersArray(arrayP);
+		printArray(arrayP);
+		assigningRandomNumbersArray(arrayQ);
+		printArray(arrayQ);
+		calculationCommonDenominator(arrayQ);
 	}
 
+	private static int arrayLengthInitialization() {
+		Random rand = new Random();
+		int length = 0;
+		length = rand.nextInt(20) + 1;
+		return length;
+	}
+	
+	private static void assigningRandomNumbersArray(int[] array) {
+		Random rand = new Random();
+		for (int i = 0; i < array.length; i++) {
+			array[i] = rand.nextInt(10) + 1;
+		}
+	}
+	
+	private static void printArray(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
+		System.out.println();
+	}
+	
+	private static void calculationCommonDenominator(int[] arrayQ) {
+		calculationCommonDivisor(arrayQ);
+	}
+	
+	private static void calculationCommonDivisor(int[] arrayQ) {
+		int number = 1;
+		boolean flag = true;
+		while (flag == true) {
+			flag = false;
+			for (int i = 0; i < arrayQ.length; i++) {
+				if (number % arrayQ[i] != 0) {
+					flag = true;
+				}
+			}
+			if (flag == true) {
+				number++;
+			}
+		}
+		System.out.println("Множитель: " + number);
+	}
+	
+	
 }
