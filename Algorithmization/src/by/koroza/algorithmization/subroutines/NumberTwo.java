f0f8961ee486d66ee46e6ce242b370e457d7949c
Alexey <koroza.alexey99@gmail.com>
2022-02-03 18:52:46 +0200

    *

--- Algorithmization/src/by/koroza/algorithmization/subroutines/NumberTwo.java
+++ Algorithmization/src/by/koroza/algorithmization/subroutines/NumberTwo.java
@@ -2,11 +2,27 @@
 
 package by.koroza.algorithmization.subroutines;
 
+import java.util.Random;
+
 public class NumberTwo {
 
 	public static void main(String[] args) {
-
-		
+		int numberA = assigningRandomNumbers();
+		int numberB = assigningRandomNumbers();
+		int numberC = assigningRandomNumbers();
+		int numberD = assigningRandomNumbers();
+		System.out.println("Первое число: " + numberA);
+		System.out.println("Второе число: " + numberB);
+		System.out.println("Третье число: " + numberC);
+		System.out.println("Четвёртое число: " + numberD);
 		
 	}
+	
+	private static int assigningRandomNumbers() {
+		int number = 0;
+		Random rand = new Random();
+		number = rand.nextInt(30) + 1;
+		return number;
+	}
+	
 }
