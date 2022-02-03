b90a770e0718665aea7f3d40df7c9ffa8a68a672
Alexey <koroza.alexey99@gmail.com>
2022-02-03 17:29:45 +0200

    *

--- Algorithmization/src/by/koroza/algorithmization/subroutines/NumberOne.java
+++ Algorithmization/src/by/koroza/algorithmization/subroutines/NumberOne.java
@@ -1,11 +1,14 @@
-
+/*Написать метод(методы) для нахождения наибольшего общего делителя и наименьшего общего кратного двух натуральных чисел: 
+ *             A * B
+ * НОК(A,B) = -------
+ *            НОД(A,B)
+ */
 
 package by.koroza.algorithmization.subroutines;
 
 public class NumberOne {
 
 	public static void main(String[] args) {
-		
 
 	}
 
