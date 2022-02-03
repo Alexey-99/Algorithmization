219b778b853dd955fa402c81e32fb3e19bbfc266
Alexey <koroza.alexey99@gmail.com>
2022-02-03 18:46:56 +0200

    *

---
+++ Algorithmization/src/by/koroza/algorithmization/subroutines/NumberTwo.java
@@ -0,0 +1,12 @@
+/*Написать метод(методы) для нахождения наибольшего общего делителя четырех натуральных чисел. */
+
+package by.koroza.algorithmization.subroutines;
+
+public class NumberTwo {
+
+	public static void main(String[] args) {
+
+		
+		
+	}
+}
