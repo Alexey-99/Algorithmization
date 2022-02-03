ad9813a2d3226e22c93f26dcd33d30286d05df7b
Alexey <koroza.alexey99@gmail.com>
2022-02-03 19:18:00 +0200

    *

---
+++ Algorithmization/src/by/koroza/algorithmization/subroutines/NumberThree.java
@@ -0,0 +1,12 @@
+/* Вычислить площадь правильного шестиугольника со стороной а, используя метод вычисления площади треугольника. */
+
+package by.koroza.algorithmization.subroutines;
+
+public class NumberThree {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
