3afd5e186d93d43b0747145db551263595bbd493
Alexey <koroza.alexey99@gmail.com>
2022-02-03 19:26:17 +0200

    *

--- Algorithmization/src/by/koroza/algorithmization/subroutines/NumberThree.java
+++ Algorithmization/src/by/koroza/algorithmization/subroutines/NumberThree.java
@@ -2,11 +2,19 @@
 
 package by.koroza.algorithmization.subroutines;
 
+import java.util.Random;
+
 public class NumberThree {
 
 	public static void main(String[] args) {
+		int sideHexagon = assigningRandomNumbers();
 		
-
 	}
 
+	private static int assigningRandomNumbers() {
+		int number = 0;
+		Random rand = new Random();
+		number = rand.nextInt(30) + 1;
+		return number;
+	}
 }
