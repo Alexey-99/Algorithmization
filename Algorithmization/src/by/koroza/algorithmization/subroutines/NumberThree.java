57ef2a41df67b1fa48b49d16a5790b7bd602ffc7
Alexey <koroza.alexey99@gmail.com>
2022-02-05 16:33:37 +0200

    *

--- Algorithmization/src/by/koroza/algorithmization/subroutines/NumberThree.java
+++ Algorithmization/src/by/koroza/algorithmization/subroutines/NumberThree.java
@@ -8,7 +8,7 @@ public class NumberThree {
 
 	public static void main(String[] args) {
 		int sideHexagon = assigningRandomNumbers();
-		
+
 	}
 
 	private static int assigningRandomNumbers() {
@@ -16,5 +16,21 @@ public class NumberThree {
 		Random rand = new Random();
 		number = rand.nextInt(30) + 1;
 		return number;
+	}
+
+	private static double calculatingAreaTriangle(int sideHexagon) {
+		int heightTriangle = 0;
+		double areaTriangle = 0.5 * sideHexagon * heightTriangle;
+
+		return areaTriangle;
+	}
+
+	private static void calculatingAreaHexagon(int sideHexagon) {
+		double heightTriangle = calculatingAreaTriangle(sideHexagon);
+
+	}
+
+	private static void calculatingHeightTriangle(int sideHexagon) {
+
 	}
 }
