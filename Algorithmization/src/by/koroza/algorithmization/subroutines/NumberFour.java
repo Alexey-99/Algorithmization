c3a3df72414a1660cf5f9d47ccf6a128facc6d73
Alexey <koroza.alexey99@gmail.com>
2022-02-05 16:53:21 +0200

    *

---
+++ Algorithmization/src/by/koroza/algorithmization/subroutines/NumberFour.java
@@ -0,0 +1,14 @@
+/*На плоскости заданы своими координатами n точек. 
+ * Написать метод(методы), определяющие, между какими из пар точек самое большое расстояние. 
+ * Указание. Координаты точек занести в массив. 
+ */
+
+package by.koroza.algorithmization.subroutines;
+
+public class NumberFour {
+
+	public static void main(String[] args) {
+
+	}
+
+}
--- Algorithmization/src/by/koroza/algorithmization/subroutines/NumberThree.java
+++ Algorithmization/src/by/koroza/algorithmization/subroutines/NumberThree.java
@@ -7,30 +7,36 @@ import java.util.Random;
 public class NumberThree {
 
 	public static void main(String[] args) {
-		int sideHexagon = assigningRandomNumbers();
-
+		int sideHexagon = assigningRandomNumbersSideHexagon();
+		System.out.println("Сторона шестиугольника равна: " + sideHexagon);
+		calculatingAreaHexagon(sideHexagon);
 	}
 
-	private static int assigningRandomNumbers() {
+	private static int assigningRandomNumbersSideHexagon() {
 		int number = 0;
 		Random rand = new Random();
 		number = rand.nextInt(30) + 1;
 		return number;
 	}
 
-	private static double calculatingAreaTriangle(int sideHexagon) {
-		int heightTriangle = 0;
-		double areaTriangle = 0.5 * sideHexagon * heightTriangle;
+	private static void calculatingAreaHexagon(int sideHexagon) {
+		double areaTriangle = calculatingAreaTriangle(sideHexagon);
+		double areaHexagon = areaTriangle * 6;
+		printAreaHexagon(areaHexagon);
+	}
 
+	private static double calculatingAreaTriangle(int sideHexagon) {
+		double heightTriangle = calculatingHeightTriangle(sideHexagon);
+		double areaTriangle = 0.5 * sideHexagon * heightTriangle;
 		return areaTriangle;
 	}
 
-	private static void calculatingAreaHexagon(int sideHexagon) {
-		double heightTriangle = calculatingAreaTriangle(sideHexagon);
-
+	private static double calculatingHeightTriangle(int sideHexagon) {
+		double heightTriangle = Math.sqrt((Math.pow(sideHexagon, 2) - Math.pow((sideHexagon / 2), 2)));
+		return heightTriangle;
 	}
 
-	private static void calculatingHeightTriangle(int sideHexagon) {
-
+	private static void printAreaHexagon(double areaHexagon) {
+		System.out.println("Площадь шестиугольника равна: " + areaHexagon);
 	}
 }
