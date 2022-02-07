c5d5b84614df67076e4ab50646ca448d2694a772
Alexey <koroza.alexey99@gmail.com>
2022-02-07 15:01:04 +0200

    *

--- Algorithmization/src/by/koroza/algorithmization/subroutines/NumberFour.java
+++ Algorithmization/src/by/koroza/algorithmization/subroutines/NumberFour.java
@@ -13,11 +13,12 @@ import java.util.Random;
 public class NumberFour {
 
 	public static void main(String[] args) {
-		
 		int amountPoints = initializationAmountPoints();
-		int[] arrayCoordinate = new int[amountPoints * 2];
-		
-		
+		int[] arrayCoordinateXAndY = new int[amountPoints * 2];
+		int planeLength = 10;
+		int[][] plane = new int[planeLength][planeLength];
+		fillingRandomNumbersArrayCoordinates(arrayCoordinateXAndY, plane);
+		calculatingMaximumDistanceBetweenPoints(arrayCoordinateXAndY);
 	}
 
 	private static int initializationAmountPoints() {
@@ -28,8 +29,28 @@ public class NumberFour {
 		} while (amountPoints < 3);
 		return amountPoints;
 	}
-	
-	private static void fillingRandomNumbersArrayCoordinates() {
-		
+
+	private static void fillingRandomNumbersArrayCoordinates(int[] arrayCoordinateXAndY, int[][] plane) {
+		Random rand = new Random();
+		for (int i = 0; i < arrayCoordinateXAndY.length; i++) {
+			arrayCoordinateXAndY[i] = rand.nextInt(plane.length - 1);
+		}
+	}
+
+	private static void calculatingMaximumDistanceBetweenPoints(int[] arrayCoordinateXAndY) {
+		double maximumDistance = 0;
+		double distance = 0;
+		for (int i = 0; i < arrayCoordinateXAndY.length; i += 2) {
+			for (int j = i; j < arrayCoordinateXAndY.length; j += 2) {
+				int xPointA = i;
+				int yPointA = i + 1;
+				int xPointB = j;
+				int yPointB = j + 1;
+				distance = sqrt(pow(xPointB - xPointA, 2) + pow(yPointB - yPointA, 2));
+				if (maximumDistance < distance) {
+					maximumDistance = distance;
+				}
+			}
+		}
 	}
 }
