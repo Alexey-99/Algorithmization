d712395a8ea8a1bf5ca8deb460b098c3dae44ee9
Alexey <koroza.alexey99@gmail.com>
2022-02-08 15:15:46 +0200

    *

---
+++ Algorithmization/src/by/koroza/algorithmization/subroutines/NumberEleven.java
@@ -0,0 +1,11 @@
+/*Написать метод(методы), определяющий, в каком из данных двух чисел больше цифр. */
+
+package by.koroza.algorithmization.subroutines;
+
+public class NumberEleven {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
--- Algorithmization/src/by/koroza/algorithmization/subroutines/NumberTen.java
+++ Algorithmization/src/by/koroza/algorithmization/subroutines/NumberTen.java
@@ -1,5 +1,6 @@
 /* Дано натуральное число N. 
- * Написать метод(методы) для формирования массива, элементами которого являются цифры числа N. */
+ * Написать метод(методы) для формирования массива, элементами которого являются цифры числа N. 
+ * */
 
 package by.koroza.algorithmization.subroutines;
 
@@ -8,14 +9,45 @@ import java.util.Random;
 public class NumberTen {
 
 	public static void main(String[] args) {
-		double number = initializationNumber();
-		
+		int number = initializationNumber();
+		System.out.println(number);
+		formingArrayWithElementsDigitsNumberAndPrintHim(number);
 	}
 
 	private static int initializationNumber() {
 		Random rand = new Random();
 		int number = 0;
-		number =  rand.nextInt();
+		do {
+			number = rand.nextInt() + 1235;
+		} while (number < 0);
 		return number;
+	}
+	
+	private static void formingArrayWithElementsDigitsNumberAndPrintHim(int number) {
+		int lengthArray = countingNumberDigits(number);
+		int[] array = new int[lengthArray];
+		for (int i = array.length - 1; i >= 0; i--) {
+			int remainder = number % 10;
+			array[i] = remainder;
+			number /= 10; 
+			}
+		printArray(array);
+		}
+	
+	private static void printArray(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
+		System.out.println();
+	}
+
+	private static int countingNumberDigits(int number) {
+		int count = 0;
+		while(number >= 10) {
+			int mod = number / 10;
+			number = mod;
+			count++;
+		}
+		return count + 1;
 	}
 }
