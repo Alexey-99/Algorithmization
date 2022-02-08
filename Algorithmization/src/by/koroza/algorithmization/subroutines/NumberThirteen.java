74a8a54687ef0dc1f19395a384871610a9f02fe5
Alexey <koroza.alexey99@gmail.com>
2022-02-08 17:39:06 +0200

    *

--- Algorithmization/src/by/koroza/algorithmization/subroutines/NumberThirteen.java
+++ Algorithmization/src/by/koroza/algorithmization/subroutines/NumberThirteen.java
@@ -6,11 +6,28 @@
 
 package by.koroza.algorithmization.subroutines;
 
+import java.util.Random;
+
 public class NumberThirteen {
 
 	public static void main(String[] args) {
-		
-
+		int minNumber = initializationNumber();
+		int maxNumber = 2 * minNumber;
+		findingAndPrintNumbersTwins(minNumber, maxNumber);
 	}
 
+	private static int initializationNumber() {
+		Random rand = new Random();
+		int number = 0;
+		do {
+			number = rand.nextInt(50);
+		} while (number <= 2);
+		return number;
+	}
+	
+	private static void findingAndPrintNumbersTwins(int minNumber, int maxNumber) {
+		for (int i = minNumber; i <= maxNumber - 2; i++) {
+			System.out.println(i + ", " + (i + 2));
+		}
+	}
 }
