8b9f6c18abbfb363a8e5c20bf2f92c953a1abb1f
Alexey <koroza.alexey99@gmail.com>
2022-02-08 16:27:48 +0200

    *

--- Algorithmization/src/by/koroza/algorithmization/subroutines/NumberEleven.java
+++ Algorithmization/src/by/koroza/algorithmization/subroutines/NumberEleven.java
@@ -2,10 +2,66 @@
 
 package by.koroza.algorithmization.subroutines;
 
+import java.util.Random;
+
 public class NumberEleven {
 
 	public static void main(String[] args) {
-		
+		int numberOne = initializationNumber();
+		int numberTwo = initializationNumber();
+		System.out.println("Первое число: " + numberOne);
+		System.out.println("Второе число: " + numberTwo);
+		maximumNumberDigits(numberOne, numberTwo);
 	}
 
+	private static void maximumNumberDigits(int numberOne, int numberTwo) {
+		int countDigitsOne = countingNumberDigits(numberOne);
+		int countDigitsTwo = countingNumberDigits(numberTwo);
+		printResult(countDigitsOne, countDigitsTwo, numberOne, numberTwo);
+	}
+
+	private static void printResult(int countDigitsOne, int countDigitsTwo, int numberOne, int numberTwo) {
+		if (countDigitsOne > countDigitsTwo) {
+			StringBuilder build = new StringBuilder();
+			build.append("В числе ").append(numberOne).append(" больше цифр, чем в числе ").append(numberTwo);
+			build.append(", так как в числе ").append(numberOne).append(" количество цифр ").append(countDigitsOne)
+					.append(", а в числе ");
+			build.append(numberTwo).append(" количество цифр ").append(countDigitsTwo);
+			System.out.println(build);
+		} else if (countDigitsTwo > countDigitsOne) {
+			StringBuilder build = new StringBuilder();
+			build.append("В числе ").append(numberTwo).append(" больше цифр, чем в числе ").append(numberOne);
+			build.append(", так как в числе ").append(numberTwo).append(" количество цифр ").append(countDigitsTwo)
+					.append(", а в числе ");
+			build.append(numberOne).append(" количество цифр ").append(countDigitsOne);
+			System.out.println(build);
+		} else if (countDigitsOne == countDigitsTwo) {
+			StringBuilder build = new StringBuilder();
+			build.append("В числах ").append(numberOne).append(" и числе ").append(numberTwo)
+					.append(" одинаковое количество цифр");
+			build.append(", так как в числе ").append(numberOne).append(" количество цифр ").append(countDigitsOne)
+					.append(", и в числе ");
+			build.append(numberTwo).append(" количество цифр ").append(countDigitsTwo);
+			System.out.println(build);
+		}
+	}
+
+	private static int initializationNumber() {
+		Random rand = new Random();
+		int number = 0;
+		do {
+			number = rand.nextInt();
+		} while (number < 0);
+		return number;
+	}
+
+	private static int countingNumberDigits(int number) {
+		int count = 1;
+		while (number >= 10) {
+			number = number / 10;
+
+			count++;
+		}
+		return count;
+	}
 }
---
+++ Algorithmization/src/by/koroza/algorithmization/subroutines/NumberTwelve.java
@@ -0,0 +1,15 @@
+/* Даны натуральные числа К и N. 
+ * Написать метод(методы) формирования массива А, элементами которого являются числа, 
+ * сумма цифр которых равна К и которые не большее N. 
+ * */
+
+package by.koroza.algorithmization.subroutines;
+
+public class NumberTwelve {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
