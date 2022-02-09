b54929136b1108943e8e75039adf93cc24e91b8f
Alexey <koroza.alexey99@gmail.com>
2022-02-09 18:48:21 +0200

    *

---
+++ Algorithmization/src/by/koroza/algorithmization/subroutines/NumberFifteen.java
@@ -0,0 +1,15 @@
+/* Найти все натуральные n-значные числа, 
+ * цифры в которых образуют строго возрастающую последовательность (например, 1234, 5789). 
+ * Для решения задачи использовать декомпозицию. 
+ * */
+
+package by.koroza.algorithmization.subroutines;
+
+public class NumberFifteen {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
