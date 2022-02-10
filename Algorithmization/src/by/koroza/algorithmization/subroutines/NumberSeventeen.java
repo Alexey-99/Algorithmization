c8d3293999c4723622dc5fc8868a822b30d2e957
Alexey <koroza.alexey99@gmail.com>
2022-02-10 19:04:04 +0200

    *

--- Algorithmization/src/by/koroza/algorithmization/subroutines/NumberSeventeen.java
+++ Algorithmization/src/by/koroza/algorithmization/subroutines/NumberSeventeen.java
@@ -11,15 +11,38 @@ public class NumberSeventeen {
 
 	public static void main(String[] args) {
 		int number = initializationNumber();
-		
+		countSubtractionSumDigits(number);
 	}
 
 	private static int initializationNumber() {
 		Random rand = new Random();
 		int number = 0;
 		do {
-			number = rand.nextInt(50);
+			number = rand.nextInt(10000);
 		} while (number < 1);
 		return number;
+	}
+	
+	private static void countSubtractionSumDigits(int number) {
+		int count = 0;
+		int sum = 0;
+		System.out.print("С числом " + number);
+		while (number > 0) {
+			sum = sum(number);
+			number -= sum;
+			count++;
+		}
+		System.out.print(" произведено " + count + " операций");
+	}
+	
+	private static int sum(int number) {
+		int remainder = 0;
+		int sum = 0;
+		while (number >= 1) {
+			remainder = number % 10;
+			sum += remainder;
+			number /= 10;
+		}
+		return sum;
 	}
 }
